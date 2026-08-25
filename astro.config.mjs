1f2817da4702566c9c1b24670db408d684f5a816
zakokun <i@wen.moe>
2026-08-25 22:17:07 +0800

    升级版本

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.zakokun.com',
+  adapter: cloudflare(),
 });
